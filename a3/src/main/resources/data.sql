31caa20a0ecde7147c4632bf8802715b33e23c86
plusdojo2026-morita <morita-sae-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-15 15:00:21 +0900

    ダミーデータ作成

---
+++ a3/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- ユーザ情報
+INSERT INTO users (mail_address, password, nickname, icon_id) VALUES
+('yamada@example.com', 'password123', 'やまだ', 1),
+('tanaka@example.com', 'pass456', 'たなか', 2);
+
+--自然ミッションの提案
