c51edc515a3f10f997aa9f70536e2fc23c977ef5
plusdojo2026-morita <morita-sae-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-15 11:28:57 +0900

    Entityクラス作成②

--- a3/src/main/java/com/example/demo/entity/Record.java
+++ a3/src/main/java/com/example/demo/entity/Record.java
@@ -1,9 +1,16 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,6 +30,11 @@ public class Record {
 	private String progress;
 	private String feel;
 	private String found;
-	private String date;
-	private Integer user_id;
+	private LocalDate date;
+	//private Integer user_id;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="user_id")
+	@JsonIgnore
+	private User user;
 }
--- a3/src/main/java/com/example/demo/entity/User.java
+++ a3/src/main/java/com/example/demo/entity/User.java
@@ -1,9 +1,14 @@
 package com.example.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,5 +26,9 @@ public class User {
 	private String mail_address;
 	private String password;
 	private String nickname;
-	private Integer icon_id;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="icon_id")
+	@JsonIgnore
+	private Icon icon;
 }
