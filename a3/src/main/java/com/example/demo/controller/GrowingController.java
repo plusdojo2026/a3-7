6cbad5652b155b585d8bddde79f0817a12056af4
plusdojo2026-kawahara <kawahara-kodai-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-17 17:02:04 +0900

    ホーム画面の基本的な機能

--- a3/src/main/java/com/example/demo/controller/GrowingController.java
+++ a3/src/main/java/com/example/demo/controller/GrowingController.java
@@ -1,10 +1,34 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
+import java.time.LocalDate;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.RecordsRepository;
+
+import jakarta.servlet.http.HttpSession;
+
+@RestController
 public class GrowingController {
+	
+	@Autowired
+	private RecordsRepository recordsRepository;
 
-//	@GetMapping("/home/")
-//	public String grow(@RequestParam)
+	@GetMapping("/api/plant")
+	public Integer counter(HttpSession session) {
+		Integer id = (Integer) session.getAttribute("loginUserId");
+		LocalDate today = LocalDate.now();
+		
+		int year = today.getYear();
+		int month = today.getMonthValue();
+		
+		LocalDate start = LocalDate.of(year, month, 1);
+		LocalDate end = start.plusMonths(1);
+		
+		int result = recordsRepository.count(id, start, end);
+		
+		return result;
+	}
 }
