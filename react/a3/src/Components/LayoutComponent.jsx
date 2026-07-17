23331adb11d81d5ae0eac53562f34d25541b9ff2
plusdojo2026-morita <morita-sae-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-17 15:34:33 +0900

    ヘッダーフッター作成、ダミーデータ編集

--- react/a3/src/Components/LayoutComponent.jsx
+++ react/a3/src/Components/LayoutComponent.jsx
@@ -1,11 +1,19 @@
+import FooterComponent from "./FooterComponent";
 import HeaderComponent from "./HeaderComponent";
+import './LayoutComponent.css'
 
 let LayoutComponent = ({children}) => {
     return(
         <div>
             <HeaderComponent></HeaderComponent>
+            <main className="main">
             {children}
+            </main>
+            <FooterComponent></FooterComponent>
         </div>
     );
 };
 export default LayoutComponent;
+
+// App.jsxに後で追加
+{/* <Route index element={<LayoutComponent><Login></Login></LayoutComponent>}></Route> */}
