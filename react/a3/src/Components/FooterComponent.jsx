ff5c0a566a94dd6f47fe3ddca18c32f345577f66
plusdojo2026-morita <morita-sae-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-22 16:14:52 +0900

    ヘッダーフッター修正

--- react/a3/src/Components/FooterComponent.jsx
+++ react/a3/src/Components/FooterComponent.jsx
@@ -10,22 +10,26 @@ import { NavLink } from 'react-router-dom';
 const FooterComponent = () => {
     return (
             <footer className="footer">
-      <NavLink to="/Home" className="nav-item">
+      <NavLink to="/Home" className={({isActive}) =>
+      isActive ? "nav-item active" : "nav-item"}>
         <img src={home} alt="ホーム" />
-        <p>ホーム</p>
+        <span>ホーム</span>
       </NavLink>
 
-      <NavLink to="/Progress" className="nav-item">
+      <NavLink to="/Progress" className={({isActive}) =>
+      isActive ? "nav-item active" : "nav-item"}>
         <img src={record} alt="頑張り記入" />
         <p>記録する</p>
       </NavLink>
 
-      <NavLink to="/Memory" className="nav-item">
+      <NavLink to="/Memory" className={({isActive})=>
+      isActive ? "nav-item active" : "nav-item"}>
         <img src={input} alt="成長記録" />
         <p>振り返り</p>
       </NavLink>
 
-      <NavLink to="/MyPage" className="nav-item">
+      <NavLink to="/MyPage" className={({isActive})=>
+      isActive ? "nav-item active" :"nav-item"}>
         <img src={mypage} alt="マイページ" />
         <p>マイページ</p>
       </NavLink>
--- react/a3/src/Components/HeaderComponent.jsx
+++ react/a3/src/Components/HeaderComponent.jsx
@@ -2,14 +2,29 @@
 import './HeaderComponent.css';
 //import axios from 'axios';
 import logout from "../assets/logout.png";
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 
 const HeaderComponent = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+
+        const result = window.confirm('ログアウトしますか？');
+
+            if (result) {
+                navigate("/");
+            }
+
+
+    };
+
     return (
         <header className="header">
             <p>🌱きょうのいっぽ</p>
-            <NavLink to="/" className="logout-icon">
+            <NavLink to="/" className="logout-icon" onClick={handleLogout}>
                 <img src={logout} alt="ログアウト" />
             </NavLink>
         </header>
