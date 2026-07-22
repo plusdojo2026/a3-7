450af4972af0c7f64aaa2936964063a12f22a7b7
plusdojo2026-morita <morita-sae-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-22 12:01:15 +0900

    フッター修正

--- react/a3/src/Components/FooterComponent.jsx
+++ react/a3/src/Components/FooterComponent.jsx
@@ -1,10 +1,10 @@
 // import {useState, useEffect} from 'react';
 import './FooterComponent.css'
 //import axios from 'axios';
-import home from "../assets/home.png";
-import record from "../assets/record.png";
-import input from "../assets/input.png";
-import mypage from "../assets/mypage.svg";
+import home from "../assets/home2.png";
+import record from "../assets/record2.png";
+import input from "../assets/input2.png";
+import mypage from "../assets/mypage.png";
 import { NavLink } from 'react-router-dom';
 
 const FooterComponent = () => {
