ecb8a19c7c52dcad90dfa50ac7f7b96a4ae27cb6
plusdojo2026-matsuda <matsuda-syunsuke-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-23 14:41:45 +0900

    日々の記録画面

--- a3/src/main/java/com/example/demo/repository/RecordsRepository.java
+++ a3/src/main/java/com/example/demo/repository/RecordsRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 
 import java.time.LocalDate;
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,12 +12,20 @@ import com.example.demo.entity.User;
 
 public interface RecordsRepository extends JpaRepository<Record, Integer> {
 
-	@Query(value = "SELECT COUNT(*) AS user_id \n"
-			+ "FROM Records \n"
-			+ "WHERE user_id = :user_id \n"
-			+ "AND date >= :startDate AND date < :endDate \n"
-			,nativeQuery = true)
-	Integer count(@Param("user_id") Integer user_id, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
-	
-	Optional<Record> findByUserAndDate(User user, LocalDate date);
+    @Query(value = """
+            SELECT COUNT(*)
+            FROM records
+            WHERE user_id = :user_id
+            AND date >= :startDate
+            AND date < :endDate
+            """, nativeQuery = true)
+    Integer count(@Param("user_id") Integer user_id,
+                  @Param("startDate") LocalDate startDate,
+                  @Param("endDate") LocalDate endDate);
+
+    //Optional<Record> findByUserAndDate(User user, LocalDate date);
+
+    // 日付で一覧取得
+    List<Record> findByDate(LocalDate date);
+    List<Record> findByUserAndDate(User user, LocalDate date);
 }
